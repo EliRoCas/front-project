fbd8cc286ef9a659d8d1d50530756fb358185cc9
Eli Rosero <elir_94@hotmail.com>
2024-05-03 20:57:08 -0500

    Header y nav config

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,64 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
-        {/* Se definen las rutas de la aplicación */}
-        <Routes>
-          {/* en el path agregamos el nombre de la ruta que verá nuestro usuario 
-          Cuando la URL coincide exactamente con "/clients/", se renderizará el componente dado*/}
-          <Route path='/clients/' element={<CompShowClients />} />
-          <Route path='/clients/add/' element={<CompAddClients />} />
-        </Routes>
-      </BrowserRouter>
+      <div className="App">
+        <header className="container">
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="/login" >
+                Login
+              </a>
+              <button
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                className="navbar-toggler"
+                data-bs-target="#navbarNav"
+                data-bs-toggle="collapse"
+                type="button"
+              >
+                <span className="navbar-toggler-icon" />
+              </button>
 
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item" >
+                    <a aria-current="page" className="nav-link active" href="/">
+                      Inicio
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/clients" >
+                      Clientes
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/products">
+                      Productos
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="/suppliers" className="nav-link" >
+                      Proveedores
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </header>
+
+        <BrowserRouter>
+          {/* Se definen las rutas de la aplicación */}
+          <Routes>
+            {/* en el path agregamos el nombre de la ruta que verá nuestro usuario 
+          Cuando la URL coincide exactamente con "/clients/", se renderizará el componente dado*/}
+            <Route path='/clients/' element={<CompShowClients />} />
+            <Route path='/clients/add/' element={<CompAddClients />} />
+          </Routes>
+        </BrowserRouter>
+
+      </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 
 // Se importa Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
+import'bootstrap/dist/js/bootstrap.bundle.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
